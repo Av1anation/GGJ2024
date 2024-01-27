f1036e822f8183d589e17468030a6c218ac55594
eric.moser <eric.moser@digipen.edu>
2024-01-26 21:31:05 -0800

    Babies first player

--- Godot Practice/Erics Shit/Player.cs
+++ Godot Practice/Erics Shit/Player.cs
@@ -7,10 +7,15 @@ public class Player : Area2D
     // private int a = 2;
     // private string b = "text";
 
+    public int Speed = 400;
+
+    public Vector2 ScreenSize;
+
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        ScreenSize = GetViewportRect().Size;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,5 +27,30 @@ public class Player : Area2D
         {
             velocity.x += 1;
         }
+        if (Input.IsActionPressed("move_Left"))
+        {
+            velocity.x -= 1;
+        }
+        if (Input.IsActionPressed("move_Up"))
+        {
+            velocity.y -= 1;
+        }
+        if (Input.IsActionPressed("move_Down"))
+        {
+            velocity.y += 1;
+        }
+
+
+        if (velocity.Length() > 0)
+        {
+            velocity = velocity.Normalized() * Speed;
+        }
+
+        Position += velocity * delta;
+        Position = new Vector2(
+            x: Mathf.Clamp(Position.x, 0, ScreenSize.x),
+            y: Mathf.Clamp(Position.y, 0, ScreenSize.y)
+            );
+
     }
 }
