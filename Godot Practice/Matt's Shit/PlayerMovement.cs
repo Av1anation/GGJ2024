41a121c44311274ca8c78e25a892e0a28ab16545
Sevithian <m.peterson@digipen.edu>
2024-01-26 22:24:16 -0800

    A bunch of test shit

--- Godot Practice/Matt's Shit/PlayerMovement.cs
+++ Godot Practice/Matt's Shit/PlayerMovement.cs
@@ -3,19 +3,49 @@ using System;
 
 public class PlayerMovement : RigidBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    [Export]
+    public int Speed = 400;
+
+    [Export]
+    public Vector2 PlayerVelocity;
+
+    public Vector2 ScreenSize;
+    AnimatedSprite PlayerSprite;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        PlayerSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        ScreenSize = GetViewportRect().Size;
+        PlayerVelocity = Vector2.Zero;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
+    public override void _PhysicsProcess(float delta)
     {
-        this.AddForce(new Vector2(0, 0), new Vector2(0, 1));
+        if (Input.IsActionPressed("Move_Right"))
+            PlayerVelocity.x += 1;
+        if (Input.IsActionPressed("Move_Left"))
+            PlayerVelocity.x -= 1;
+        if (Input.IsActionPressed("Move_Up"))
+            PlayerVelocity.y -= 1; // Assuming up is negative on the y-axis
+        if (Input.IsActionPressed("Move_Down"))
+            PlayerVelocity.y += 1;
+
+        PlayerVelocity = PlayerVelocity.Normalized() * Speed * delta; // Apply delta to make movement frame rate independent
+        LinearVelocity = PlayerVelocity;
+
+        // If there is no movement, let's ensure that we're not playing any 'walk' animation
+        if (PlayerVelocity.Length() == 0 || PlayerSprite != null)
+        {
+            PlayerSprite.Stop();
+            PlayerVelocity = Vector2.Zero;
+        }
+        else if (PlayerSprite != null)
+        {
+            // Play your walk animation here
+            PlayerSprite.Play();
+        }
     }
+
 }
