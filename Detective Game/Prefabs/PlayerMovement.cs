9a274f97849dd465c59f247ac3aaf9e240f6cdd2
Sevithian <m.peterson@digipen.edu>
2024-01-27 14:47:25 -0800

    Player

--- Detective Game/Prefabs/PlayerMovement.cs
+++ Detective Game/Prefabs/PlayerMovement.cs
@@ -3,36 +3,38 @@ using System;
 
 public partial class PlayerMovement : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	[Export]
+	public float Speed = 300.0f;
+	[Export]
+	public Vector2 InputVelocity;
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+		MovePlayer();
+	}
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
+	private void MovePlayer()
+	{
+		Vector2 velocity = InputVelocity;
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-
-		Velocity = velocity;
+		Velocity = velocity * Speed;
 		MoveAndSlide();
+	}
+
+	private void GetInputs()
+	{
+		if (Input.IsActionPressed("move_Up"))
+			InputVelocity.Y = 1;
+		else if (Input.IsActionPressed("move_Down"))
+			InputVelocity.Y = -1;
+		else
+			InputVelocity.Y = 0;
+
+		if (Input.IsActionPressed("move_Right"))
+			InputVelocity.X = 1;
+		else if (Input.IsActionPressed("move_Left"))
+			InputVelocity.X = -1;
+		else
+			InputVelocity.X = 0;
 	}
 }
