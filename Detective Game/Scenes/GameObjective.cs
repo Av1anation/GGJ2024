24e03ab8248b076f50c3b4511ebed31f96773f6d
Sevithian <m.peterson@digipen.edu>
2024-01-28 15:24:49 -0800

    Doing objective stuff

--- Detective Game/Scenes/GameObjective.cs
+++ Detective Game/Scenes/GameObjective.cs
@@ -3,8 +3,23 @@ using System;
 
 public partial class GameObjective : Node2D
 {
+	[Export]
+	Conditional VictoryCondition;
+
+	[Signal]
+	public delegate void OnGameFinishedEventHandler();
+	[Signal]
+	public delegate void OnGameUnfinishedEventHandler();
+
 	public void CheckProgress()
 	{
-		
+		if(VictoryCondition.Status)
+		{
+            EmitSignal(SignalName.OnGameFinished);
+        }
+		else
+		{
+            EmitSignal(SignalName.OnGameUnfinished);
+        }
 	}
 }
